35f46b08e6ff9e39f2a1896ee3076f339dae1bf5
Amiru Weerathunga <amiru.upek.weerathunga@gmail.com>
2024-10-21 14:28:12 +0530

    Css Update

--- customized-extensions/coverArtAmbient.js
+++ customized-extensions/coverArtAmbient.js
@@ -60,6 +60,7 @@ function waitForSpicetify() {
         
         if (!ambientContainer) {
             const nowPlayingBar = document.querySelector('.main-nowPlayingBar-nowPlayingBar');
+
             if (!nowPlayingBar) {
                 console.warn('Now Playing Bar container not found.');
                 return null;
@@ -69,15 +70,15 @@ function waitForSpicetify() {
             ambientContainer.id = 'ambient-container';
 
             ambientContainer.style.position = 'absolute';
-            ambientContainer.style.top = '15px';
-            ambientContainer.style.left = '12px';
-            ambientContainer.style.width = '56px';
-            ambientContainer.style.height = '56px';
+            ambientContainer.style.top = '13px';
+            ambientContainer.style.left = '10px';
+            ambientContainer.style.width = '60px';
+            ambientContainer.style.height = '60px';
             ambientContainer.style.zIndex = '-1';
             ambientContainer.style.borderRadius = 'var(--border-radius-3)';
             ambientContainer.style.backgroundSize = 'cover';
             ambientContainer.style.backgroundPosition = 'center center';
-            ambientContainer.style.filter = 'blur(10px) brightness(80%)';
+            ambientContainer.style.filter = 'blur(30px) brightness(120%)';
             ambientContainer.style.transform = 'scale(0)';
 
             nowPlayingBar.appendChild(ambientContainer);
