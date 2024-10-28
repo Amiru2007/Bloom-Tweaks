39b43ed3678b1bd555b2fd8bbc87c4ca09950d3f
Amiru Weerathunga <amiru.upek.weerathunga@gmail.com>
2024-10-28 22:55:58 +0530

    css update

--- bloomTweaks.js
+++ bloomTweaks.js
@@ -143,7 +143,7 @@ function waitForSpicetify() {
                 clearInterval(tryAddButton);
 
                 const button = document.createElement("button");
-                button.className = "Button-sc-1dqy6lx-0 Button-buttonTertiary-small-small-useBrowserDefaultFocusStyle-condensedAll encore-text-body-small-bold";
+                button.className = "bloom-tweaks Button-sc-1dqy6lx-0 Button-buttonTertiary-small-small-useBrowserDefaultFocusStyle-condensedAll encore-text-body-small-bold";
                 button.setAttribute("aria-label", "Customizations");
 
                 const icon = document.createElement("svg");
@@ -152,7 +152,7 @@ function waitForSpicetify() {
                 icon.setAttribute("aria-hidden", "true");
                 icon.setAttribute("viewBox", "0 0 16 16");
                 icon.className = "Svg-sc-ytk21e-0 Svg-img-icon-small";
-                icon.innerHTML = `<path d="M8 1.5a4 4 0 0 0-4 4v3.27a.75.75 0 0 1-.1.373L2.255 12h11.49L12.1 9.142a.75.75 0 0 1-.1-.374V5.5a4 4 0 0 0-4-4zm-5.5 4a5.5 5.5 0 0 1 11 0v3.067l2.193 3.809a.75.75 0 0 1-.65 1.124H10.5a2.5 2.5 0 0 1-5 0H.957a.75.75 0 0 1-.65-1.124L2.5 8.569V5.5zm4.5 8a1 1 0 1 0 2 0H7z"></path>`;
+                icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bolt"><path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"/><circle cx="12" cy="12" r="4"/></svg>`;
                 button.appendChild(icon);
 
                 button.addEventListener("click", () => {
@@ -203,6 +203,7 @@ function waitForSpicetify() {
                                 </label>
                             </div>
                         </div>
+                        <h3 style="padding-top: 16px;">Custom Apps Customizations</h3>
                         <div class="bloom-tweaks x-settings-row">
                             <div class="bloom-tweaks x-settings-firstColumn">
                                 <label class="bloom-tweaks encore-text encore-text-body-small encore-internal-color-text-subdued">Tag Styling (Playlist Tags Custom App)</label>
